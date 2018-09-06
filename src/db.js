43225b667ef85af04ff81d3c706f4cb989abb261
Seth Deegan <jayandseth@gmail.com>
2018-09-06 12:01:58 -0500

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,13 +1,19 @@
 const { Client } = require('pg')
-
 const con = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
 });
 
-await client.connect();
+con.connect();
 
-con.query(queryString, (err, res) => {
-  console.log(res);
-  await client.end();
-});
+function register(name, username, email, password) {
+  con.query(`INSERT INTO users (name, username, email, password) VALUES (${name}, ${username}, ${email}, crypt(${password}, gen_salt('bf', 8)));`, (err, res) => {
+    return res;
+  });
+}
+
+function login(username, email, password) {
+  con.query(`SELECT * FROM users WHERE email = lower(${email}) OR username = lower(${username}) AND password = crypt(${password}, password);`, (err, res) => {
+    return res;
+  });
+}
