c82fe7ce7658b3211336edca449144760858a557
Seth Deegan <jayandseth@gmail.com>
2018-09-09 13:05:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const { Client } = require('pg')
-
-const con = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-con.connect();
-
-con.query("SELECT username FROM users;", (err, res) => {
-  console.log(res);
-  con.end();
-});
+const db = require('./db');
