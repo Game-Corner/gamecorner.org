0bdc5f07e03fc94c844a32d65fb99d646eaee4c8
Seth Deegan <jayandseth@gmail.com>
2018-09-09 22:52:30 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+/*
 const db = require('./db');
 
 console.log(db.login());
+*/
+
+const { Client } = require('pg')
+const con = new Client({
+  connectionString: process.env.DATABASE_URL,
+});
+
+con.connect();
+
+con.query('SELECT * FROM users;', (err, res) => {
+  console.log(res);
+});
+
+con.end();
