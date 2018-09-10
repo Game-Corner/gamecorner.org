42c8cb35c861c4bd6ef205baf0fd4c219e7626a5
Seth Deegan <jayandseth@gmail.com>
2018-09-10 07:40:54 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-/*	
 const db = require('./db');
- console.log(db.login());
+console.log(db.login());
+
+/*
+const { Client } = require('pg')
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+});
+
+client.connect();
+
+client.query('INSERT INTO users (username) VALUES (\'yay\');', (err, res) => {
+  if (err) {
+    console.log(err.stack)
+  } else {
+    console.log(res.rows[0])
+  }
+  client.end();
+});
 */	
-
-const { Client } = require('pg')	
-const client = new Client({	
-  connectionString: process.env.DATABASE_URL,	
-});	
-
-client.connect();	
-
-client.query('INSERT INTO users (username) VALUES (\'yay\');', (err, res) => {	
-  if (err) {	
-    console.log(err.stack)	
-  } else {	
-    console.log(res.rows[0])	
-  }	
-  client.end();	
-});	
