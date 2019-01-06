f5a2b76101fa34f3aae04e8d0b4a93806c71aaea
Seth Deegan <jayandseth@gmail.com>
2019-01-06 14:39:28 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const db = require('./db');
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+
+app.use(express.static('pages'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
