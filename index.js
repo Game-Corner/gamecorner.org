0be69c8f151e37e91b92a1ae25dbb05063c64b43
Seth Deegan <jayandseth@gmail.com>
2019-01-06 14:39:46 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT
 
-app.use(express.static('pages'))
+app.use(express.static('/pages'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
