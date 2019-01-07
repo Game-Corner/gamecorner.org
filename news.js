ad4fd732d119a0fc785caf1f1ba469eb76a256d0
Seth Deegan <jayandseth@gmail.com>
2019-01-07 08:50:29 -0600

    Create news.js

---
+++ news.js
@@ -0,0 +1,36 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+function Repeat(props) {
+  let items = [];
+  for (let i = 0; i < props.num; i++) {
+    items.push(props.children);
+  }
+  return items;
+}
+
+function Calendar(props) {
+  return (
+    <div className='Calendar'>
+      <h1>{props.name}'s Calendar</h1>
+      <div id='box'>
+        <Repeat num={31}>
+          <div id='inner'></div>
+        </Repeat>
+      </div>
+    </div>
+  );
+}
+
+const team = {
+  name: 'Game Corner',
+  days: 31
+};
+
+ReactDOM.render(
+  <Calendar 
+    name={team.name}
+    days={team.days}
+  />,
+  document.getElementById('root')
+);
